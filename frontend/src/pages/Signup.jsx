89b035d800c7bd71e1e37e8aa2bf2b7156fc6b09
Sweety Kumari <84958437+sweety-singhh@users.noreply.github.com>
2025-05-03 00:52:16 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -28,7 +28,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = "http://localhost:8080/auth/signup";
+            const url = "https://mern-auth-u6db.onrender.com/auth/signup";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
