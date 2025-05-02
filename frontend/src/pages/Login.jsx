8f16581a547b21420c44b8c9ce14ca9a79e0dbcd
Sweety Kumari <84958437+sweety-singhh@users.noreply.github.com>
2025-05-03 00:51:50 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -28,7 +28,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = "http://localhost:8080/auth/login";
+            const url = "https://mern-auth-u6db.onrender.com/auth/login";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
