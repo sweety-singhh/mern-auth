9b35950655923f8735fd825d0908ed9a44c94792
Sweety Kumari <84958437+sweety-singhh@users.noreply.github.com>
2025-05-03 00:51:13 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = "http://localhost:5173/products";
+            const url = "https://mern-auth-u6db.onrender.com/products";
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
